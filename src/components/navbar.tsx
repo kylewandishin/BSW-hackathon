755de13ad2893afb4d0232907a6669a346a549e2
Grayson Richard <graydragon11@gmail.com>
2024-05-16 11:29:06 -0600

    DONT FUCKING TOUCH THIS COMMIT ITS SPECIAL AND WORKS.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -45,7 +45,10 @@ export default function Topbar() {
   return (
     <div className="bg-white shadow-md h-[4rem] flex items-center justify-between px-4 md:px-8 w-full fixed top-0 z-10">
       <div className="flex items-center space-x-4">
-        <Link to="/" className="flex items-center space-x-2 border-r border-gray-300 pr-4">
+        <Link
+          to="/"
+          className="flex items-center space-x-2 border-r border-gray-300 pr-4"
+        >
           <div className="w-8 h-8 md:w-12 md:h-12 bg-gray-300 flex items-center justify-center rounded-full">
             <img className="md:w-14 w-10" src={logo} alt="logo" />
           </div>
